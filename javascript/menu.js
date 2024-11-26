36a1712f224251de89f6fc8971391d22effe0a93
101dal <101dalcontact@gmail.com>
2024-11-26 20:23:46 +0100

    Reset project

--- javascript/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const mobileMenuButton = document.querySelector(".mobile-menu-button");
-  const mobileMenuList = document.querySelector(".mobile-menu-list");
-
-  mobileMenuButton.addEventListener("click", function () {
-    mobileMenuList.classList.toggle("open");
-    mobileMenuButton.classList.toggle("open"); /* Toggle open class on button */
-  });
-});
