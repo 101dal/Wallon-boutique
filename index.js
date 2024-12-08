542e4bccb458e8a16647858a24c199ddb483dd18
101dal <101dalcontact@gmail.com>
2024-12-08 17:33:33 +0100

    Added panier gestion+register

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ app.use(async (req, res, next) => {
     const response = await fetch(`http://localhost:3000/api/v1/users/me`, {
         method: 'GET',
         credentials: 'include',
-        headers: {
-            'Cookie': `token_cookie=${req.cookies.token_cookie}`
-        }
+        headers: { 'Cookie': `token_cookie=${req.cookies.token_cookie}` }
     });
     const data = await response.json();
 
@@ -34,20 +32,24 @@ app.use(async (req, res, next) => {
 
 
 
-// LITE DE TOUTES LES ROUTES
+// LISTE DE TOUTES LES ROUTES
 app.get("/", (req, res) => {
     const registered = req.query.registered === 'true';
     const disconnected = req.query.disconnected === 'true';
-    res.render("pages/home", { active: "home", registered, disconnected });
+    res.render("pages/home", { active: "home", container: "home", registered, disconnected });
 });
 
 
 app.get("/contact", (req, res) => {
-    res.render("pages/contact", { active: "contact" })
+    res.render("pages/contact", { active: "contact", container: "contact" })
 });
 
 app.get("/login", (req, res) => {
-    res.render("pages/login", { active: "login" })
+    res.render("pages/login", { active: "login", container: "auth" })
+});
+
+app.get("/register", (req, res) => {
+    res.render("pages/register", { active: false, container: "auth" })
 });
 
 app.get("/profile", async (req, res) => {
@@ -58,24 +60,35 @@ app.get("/profile", async (req, res) => {
     // Make the request to the api to get the user's information
     let response = await fetch(`http://localhost:3000/api/v1/users/me`, {
         method: 'GET',
-        headers: {
-            'Cookie': `token_cookie=${req.cookies.token_cookie}`
-        }
+        headers: { 'Cookie': `token_cookie=${req.cookies.token_cookie}` }
     });
     const infos = await response.json();
 
     // Get the user's orders
     response = await fetch(`http://localhost:3000/api/v1/orders/history`, {
         method: 'GET',
-        headers: {
-            'Cookie': `token_cookie=${req.cookies.token_cookie}`
-        }
+        headers: { 'Cookie': `token_cookie=${req.cookies.token_cookie}` }
     });
     const orders = await response.json();
 
+    res.render("pages/profile", { active: "profile", container: "profile", infos, orders });
+});
 
-    res.render("pages/profile", { active: "profile", infos, orders: orders.content });
-})
+app.get("/panier", async (req, res) => {
+    if (!res.locals.logged) {
+        res.redirect("/login");
+    }
+
+    // Get the user's carts
+    let response = await fetch(`http://localhost:3000/api/v1/carts/get`, {
+        method: 'GET',
+        headers: { 'Cookie': `token_cookie=${req.cookies.token_cookie}` }
+    });
+    const carts = await response.json();
+
+
+    res.render("pages/panier", { active: "panier", container: "cart", carts: carts })
+});
 
 
 
