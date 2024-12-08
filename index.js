78156bd4411cabe7acc5089ddd7081c82e9df37c
101dal <101dalcontact@gmail.com>
2024-12-08 16:40:24 +0100

    Ajout des commandes sur le profile

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ app.use(cookieParser());
 
 
 // Checks if the user is logged in for each request to the pages
-app.use(async (req , res, next) => {
+app.use(async (req, res, next) => {
     const response = await fetch(`http://localhost:3000/api/v1/users/me`, {
         method: 'GET',
         credentials: 'include',
-        headers: { 
+        headers: {
             'Cookie': `token_cookie=${req.cookies.token_cookie}`
-         }
+        }
     });
     const data = await response.json();
 
     res.locals.logged = data.status === 200;
-    
+
     next();
 })
 
@@ -55,16 +55,25 @@ app.get("/profile", async (req, res) => {
     }
 
     // Make the request to the api to get the user's information
-    const response = await fetch(`http://localhost:3000/api/v1/users/me`, {
+    let response = await fetch(`http://localhost:3000/api/v1/users/me`, {
         method: 'GET',
-        credentials: 'include',
-        headers: { 
+        headers: {
             'Cookie': `token_cookie=${req.cookies.token_cookie}`
-         }
+        }
     });
-    const data = await response.json();
+    const infos = await response.json();
 
-    res.render("pages/profile", { active: "profile", infos: data.content });
+    // Get the user's orders
+    response = await fetch(`http://localhost:3000/api/v1/orders/history`, {
+        method: 'GET',
+        headers: {
+            'Cookie': `token_cookie=${req.cookies.token_cookie}`
+        }
+    });
+    const orders = await response.json();
+
+
+    res.render("pages/profile", { active: "profile", infos, orders: orders.content });
 })
 
 
